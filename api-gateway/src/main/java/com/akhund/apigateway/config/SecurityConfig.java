3b719ff8925234124420fb6fb37eacfc274e2f55
birdman14 <akhund.shemsedinov@gmail.com>
2023-11-28 16:16:52 +0400

    Refactored. Added API gateway

---
+++ api-gateway/src/main/java/com/akhund/apigateway/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.akhund.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity httpSecurity) {
+        httpSecurity.csrf(c -> c.disable())
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/eureka/**")
+                        .permitAll()
+                        .anyExchange()
+                        .authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt
+                                .jwkSetUri("http://localhost:8181/realms/springboot-microservices-realm/protocol/openid-connect/certs")));
+
+        return httpSecurity.build();
+    }
+}
--- product-service/src/main/java/com/akhund/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/akhund/productservice/ProductServiceApplication.java
@@ -2,9 +2,10 @@ package com.akhund.productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
